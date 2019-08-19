dac6a7bc0f8e0b2a4579b819e4c03a65f9688a00
Gavin Dreyer <gavindreyer@Gavins-MacBook-Pro.local>
2019-08-19 14:44:18 -0700

    Finished stretch goals for function-conversion.js & object.js

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -16,7 +16,6 @@
 // Stretch
 
 // exampleArray = [1,2,3,4];
-// const triple = exampleArray.map(function (num) {
-//   return num * 3;
-// });
+//const triple = exampleArray.map((num) => num * 3);
 // console.log(triple);
+
